ddee9dd8009bb5c6cb9f30929bac569659997855
Federico Hosen <federico.hosen@restorando.com>
2018-04-08 13:31:09 -0300

    Add hello as home method, and add user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: 'Este es el tuto de Fede y su gran amigo Rails, juntos hicieron apps, grandes grandes grandes! '
+  end
+
 end
